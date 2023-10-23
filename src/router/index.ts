54ac2a4afcf431bf767693490c231e2a6d965dcf
paulabssantos <paulablsantos7@gmail.com>
2023-10-23 14:02:22 -0400

    feat: define e organiza rotas de projeto

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
+import FormularioProjetos from '../views/FormularioProjetos.vue'
+import ListaProjetos from '../views/ListaProjetos.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,9 +12,26 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: '/projetos',
-    name: 'Parefas',
-    component: Projetos
-  },
+    component: Projetos,
+    children: [
+      {
+        path: '',
+        name: 'Projeto',
+        component: ListaProjetos
+      },
+      {
+        path: 'novo',
+        name: 'Novo Projeto',
+        component: FormularioProjetos
+      },
+      {
+        path: ':id',
+        name: 'Editar Projeto',
+        component: FormularioProjetos,
+        props: true
+      },
+    ]
+  }
 ];
 
 const router = createRouter({
